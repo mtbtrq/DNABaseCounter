b7a48222ec456fb69dc2d27404b6aeec38b15625
Mutayyab Tariq <83178953+mutyyab@users.noreply.github.com>
2022-01-17 09:17:55 -0800

    Made it prettier 😳

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import sys
-from matplotlib import pyplot as plt
 
 sequence = input("Enter the DNA Sequence: ")
 
 if len(sequence) < 1:
-    sequence = "ATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGC   C"
+    sequence = "ATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGCATGC   CGCGCGCGCGCGCGCGATTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
     print(f"Since you didn't choose any DNA sequence, you will be using the default sequence: '{sequence}'")
 
 sequence = sequence.strip()
@@ -12,7 +11,6 @@ sequence = sequence.replace(" ", "")
 sequence = sequence.upper()
 
 invalidLetters = ["B", "D", "E", "F", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "U", "V", "W", "X", "Y", "Z"]
-
 for letter in invalidLetters:
     if letter in sequence:
         print(f"Invalid characters detected in DNA sequence: {letter}")    
@@ -31,9 +29,20 @@ print(f"There are {baseDict['T']} thymine.")
 print(f"There are {baseDict['G']} guanine.")
 print(f"There are {baseDict['C']} cytosine.")
 
-plt.plot(baseDict.keys(), baseDict.values())
-plt.xlabel("Base")
-plt.ylabel("Count")
+if input("Save a plotted graph to visualize the data? (y/n): ").lower() == "y":
+    try:
+        from matplotlib import pyplot as plt
+    except ImportError:
+        import os
+        os.system("pip install matplotlib")
+        from matplotlib import pyplot as plt
 
-plt.savefig('figure.png')
-print("Saved Image!")
+    plt.plot(baseDict.keys(), baseDict.values(), "k--", marker=".")
+    plt.grid(True)
+
+    plt.title("DNA Base Count")
+    plt.xlabel("Base")
+    plt.ylabel("Count")
+    plt.savefig('figure.png')
+
+    print("Saved Image!")
